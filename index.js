1f7b9b85c4b40a4b2b4f6986cc372998184d4814
NARENDARKOREM <narendar.korem@innoitlabs.com>
2024-11-29 18:09:25 +0530

    Connection established

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const dotEnv = require("dotenv");
+const sequelize = require("./db")
 const PORT = process.env.PORT || 5000;
+dotEnv.config();
 
 app.get('/',(req,res)=>{
     res.send("Server is Running");
 })
+
+sequelize.sync()
+    .then(() => {
+        console.log('Database & tables created!');
+    })
+    .catch(err => {
+        console.error('Unable to create the database:', err);
+    });
 
 app.listen(PORT,()=>{
     console.log(`Server is Running on PORT http://localhost:${PORT}`);
