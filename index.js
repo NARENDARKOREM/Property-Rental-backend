51b8d8f46d9145c9bd4a78c7974c3179057c2897
NARENDARKOREM <narendar.korem@innoitlabs.com>
2024-12-02 10:34:22 +0530

    Models created upto Notifications

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const sequelize = require("./db")
 const PORT = process.env.PORT || 5000;
 dotEnv.config();
 
+const Admin = require("./models/Admin");
+const PayoutSetting = require("./models/PayoutSetting");
+const PlanPurchaseHistory = require("./models/PlanPurchaseHistory");
+const TblBook = require("./models/TblBook");
+const TblCategory = require("./models/TblCategory");
+const TblCountry = require('./models/TblCountry');
+const TblCoupon = require('./models/TblCoupon');
+const TblEnquiry = require('./models/TblEnquiry');
+const TblExtra = require('./models/TblExtra');
+const TblFacility = require("./models/TblFacility");
+const TblFaq = require('./models/TblFaq');
+const TblFav = require('./models/TblFav');
+const TblGallery = require('./models/TblGallery');
+const TblGalCat = require('./models/TblGalCat');
+const TblNotification =require('./models/TblNotification');
+
 app.get('/',(req,res)=>{
     res.send("Server is Running");
 })
---
+++ models/TblNotification.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db'); 
+
+const TblNotification = sequelize.define('TblNotification', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  uid: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  datetime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+}, {
+  tableName: 'tbl_notification',
+  timestamps: true,
+  paranoid: true,
+  charset: 'latin1'
+});
+
+module.exports = TblNotification;
