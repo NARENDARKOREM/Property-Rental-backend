bf1cea003cef9a330f4de57a977150b7e3a13a2b
Suresh63005 <193j5a0335@raghuinstech.com>
2024-12-05 16:41:19 +0530

    changes

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-});
+    dialect: process.env.DB_DIALECT || 'mysql',
+    port: process.env.DB_PORT      
+  });
 
 sequelize.authenticate()
     .then(() => {
