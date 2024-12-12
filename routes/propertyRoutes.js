98424a868e84e27c9bcbf2ced0ffe2a0f1c549d1
rajesh <rajesh.swain@innoitlabs.com>
2024-12-12 10:14:04 +0530

    changes

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -4,9 +4,11 @@ const propertyController = require("../controllers/propertyController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 
+
 router.post('/upsert', adminMiddleware.isAdmin, propertyController.upsertProperty);
 router.get('/', propertyController.getAllProperties);
 router.get('/:id',authMiddleware.isAuthenticated, propertyController.getPropertyById);
 router.delete('/delete/:id', adminMiddleware.isAdmin, propertyController.deleteProperty);
+
 
 module.exports = router;
