b85d91b48568f8a3c7b7e1a55da87be63f6ab0ee
Suresh63005 <193j5a0335@raghuinstech.com>
2024-12-19 12:16:30 +0530

    cors added

--- index.js
+++ index.js
@@ -74,9 +74,11 @@ dotEnv.config();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
