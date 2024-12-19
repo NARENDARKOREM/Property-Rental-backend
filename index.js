d2da0bae8383467680cdef643ac32bbe35d4803c
Narendar2024 <narendarkorem471@gmail.com>
2024-12-19 11:15:35 +0530

    userlist status

--- index.js
+++ index.js
@@ -77,11 +77,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:5173"],
+    // origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(
   session({
