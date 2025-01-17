6a06d7214d36fef30c2020ed43e4922e241b5b2e
Narendar2024 <narendarkorem471@gmail.com>
2025-01-17 10:11:14 +0530

    Add host traveler review controller; update property controller to parse and store standard rules during property editing.

---
+++ userControllers/host_traveler_reivew_controller.js
@@ -0,0 +1,7 @@
+const HostTravelerReview = require('../models/HostTravelerReview');
+const User = require('../models/User');
+const TblBook = require('../models/TblBook');
+
+const hostTravelerReview = async(req, res)=>{
+    const uid = req.user.id;
+}
