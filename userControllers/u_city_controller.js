d3516f4b98806cca7cbeb6c0c08b3c859e77f59f
Narendar2024 <narendarkorem471@gmail.com>
2025-01-30 12:11:46 +0530

    Add block_start and block_end fields to Property and TblBook models; update booking logic to handle blocked dates

--- userControllers/u_city_controller.js
+++ userControllers/u_city_controller.js
@@ -2,14 +2,49 @@ const { where } = require("sequelize");
 const TblCity = require("../models/TblCity");
 const TblCountry = require("../models/TblCountry");
 
+// const getActiveCities = async (req, res) => {
+//   try {
+//     const cities = await TblCity.findAll({
+//       where: { status: 1 },
+//       //   attributes: ["id", "title"],
+//       include: [
+//         {
+//           model: TblCountry,
+//           attributes: ["title"],
+//           where: { deletedAt: null },
+//           required: true,
+//         },
+//       ],
+//     });
+
+//     if (!cities || cities.length === 0) {
+//       return res.status(400).json({ error: "Cities not found" });
+//     }
+
+//     const formattedCities = cities.map((city) => ({
+//       id: city.id,
+//       title: `${city.title}  (${city.country.title})`,
+//     }));
+
+//     return res.status(200).json({
+//       message: "Cities Fetched Successfully",
+//       formattedCities,
+//     });
+//   } catch (error) {
+//     console.error("Error fetching cities", error);
+//     return res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
+
 const getActiveCities = async (req, res) => {
+  const { country_id } = req.body;
   try {
     const cities = await TblCity.findAll({
-      where: { status: 1 },
-      //   attributes: ["id", "title"],
+      where: { status: 1, country_id: country_id },
       include: [
         {
           model: TblCountry,
+          as: "country", // Use the alias here
           attributes: ["title"],
           where: { deletedAt: null },
           required: true,
@@ -23,7 +58,8 @@ const getActiveCities = async (req, res) => {
 
     const formattedCities = cities.map((city) => ({
       id: city.id,
-      title: `${city.title}  (${city.country.title})`,
+      title: `${city.title} (${city.country.title})`, // Access the country title using the alias
+      img:city.img
     }));
 
     return res.status(200).json({
@@ -36,4 +72,6 @@ const getActiveCities = async (req, res) => {
   }
 };
 
+
 module.exports = { getActiveCities };
+
