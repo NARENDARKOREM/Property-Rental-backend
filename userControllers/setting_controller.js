6b2de27f37533e05c979c8c71f37d36831768e94
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-03-17 14:52:52 +0530

    changes

--- userControllers/setting_controller.js
+++ userControllers/setting_controller.js
@@ -17,4 +17,32 @@ const SettingAPI = async(req,res)=>{
     }
 }
 
-module.exports = {SettingAPI}
+const updateSetting = async(req,res)=>{
+    const uid = req.user.id;
+    if(!uid){
+        return res.status(401).json({message:"User not foud"});
+    }
+    const {privacy_policy,terms_conditions}=req.body;
+    try {
+        const setting = await Setting.findOne();
+        if(!setting){
+            return res.status(404).json({message:"Setting not found"});
+        }
+        setting.privacy_policy=privacy_policy ?? setting.privacy_policy;
+        setting.terms_conditions=terms_conditions ?? setting.terms_conditions;
+
+        await setting.save();
+        return res.status(200).json({
+            message:"Setting updated Successfully",
+            setting:{
+                privacy_policy:privacy_policy,
+                terms_conditions:terms_conditions,
+            }
+        })
+    } catch (error) {
+        console.error("Error Occurs While updating Settings: ",error);
+        return res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+}
+
+module.exports = {SettingAPI,updateSetting}
--- userRoutes/setting_routes.js
+++ userRoutes/setting_routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const settingController = require('../userControllers/setting_controller');
 const authMiddleware = require('../middlewares/authMiddleware');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
 router.get("/settings",authMiddleware.isAuthenticated,settingController.SettingAPI);
+router.patch('/update-settings',adminMiddleware.isAdmin,settingController.updateSetting)
 
 module.exports = router;
