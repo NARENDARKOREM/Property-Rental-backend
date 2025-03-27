ba4f6d034e6a967b1a758c9ff1329a1ba8be0c7a
SureshPyla2000 <suresh.pyla@innoitlabs.com>
2025-03-27 10:30:26 +0530

    changes in property requests

--- userRoutes/host_traveler_review_routes.js
+++ userRoutes/host_traveler_review_routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const hostTravelerReviewController = require('../userControllers/host_traveler_reivew_controller');
 const hostMiddleware = require('../middlewares/authMiddleware');
 
-router.post("/post-review", hostMiddleware.isHost,hostTravelerReviewController.hostTravelerReview );
-router.get("/traveler-reviews",hostMiddleware.isHost, hostTravelerReviewController.getHostTravelerReviews)
+router.post("/post-review", hostMiddleware.isAuthenticated,hostTravelerReviewController.hostTravelerReview );
+router.get("/traveler-reviews",hostMiddleware.isAuthenticated, hostTravelerReviewController.getHostTravelerReviews)
 
 module.exports = router;
